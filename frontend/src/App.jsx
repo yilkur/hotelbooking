44caf02167b54e69d1faef2ccff019ccba0dde83
yilmazkurnazcan <yilmaz.kurnazcan@live.de>
2026-07-24 16:07:21 +0300

    add icons to nav

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,12 @@ import Categories from "./Categories.jsx";
 import Guests from "./Guests.jsx";
 import Rooms from "./Rooms.jsx";
 import Hotels from "./Hotels.jsx";
+import EventAvailable from '@mui/icons-material/EventAvailable';
+import KingBed from '@mui/icons-material/KingBed';
+import HomeIcon from '@mui/icons-material/Home';
+import HotelIcon from '@mui/icons-material/Hotel';
+import PersonIcon from '@mui/icons-material/Person';
+import CategoryIcon from '@mui/icons-material/Category';
 
 const App = () => {
     const location = useLocation();
@@ -17,12 +23,13 @@ const App = () => {
             <CssBaseline/>
             <Box sx={{borderBottom: 1, borderColor: 'divider'}}>
                 <Tabs onChange={handleChange} aria-label="basic tabs example" value={location.pathname}>
-                    <Tab label="Home" value="/" component={Link} to="/"/>
-                    <Tab label="Buchungen" value="/bookings" component={Link} to="/bookings"/>
-                    <Tab label="Hotels" value="/hotels" component={Link} to="/hotels"/>
-                    <Tab label="Gäste" value="/guests" component={Link} to="/guests"/>
-                    <Tab label="Räume" value="/rooms" component={Link} to="/rooms"/>
-                    <Tab label="Kategorien" value="/categories" component={Link} to="/categories"/>
+                    <Tab label="Home" icon={<HomeIcon />} iconPosition="start" value="/" component={Link} to="/"/>
+                    <Tab label="Buchungen" icon={<EventAvailable />} iconPosition="start" value="/bookings" component={Link} to="/bookings"/>
+                    <Tab label="Hotels" icon={<HotelIcon />} iconPosition="start" value="/hotels" component={Link} to="/hotels"/>
+                    <Tab label="Gäste" icon={<PersonIcon />} iconPosition="start" value="/guests" component={Link} to="/guests"/>
+                    <Tab label="Räume" icon={<KingBed />} iconPosition=
+                        "start" value="/rooms" component={Link} to="/rooms"/>
+                    <Tab label="Kategorien" icon={<CategoryIcon />} iconPosition="start" value="/categories" component={Link} to="/categories"/>
                 </Tabs>
             </Box>
 
