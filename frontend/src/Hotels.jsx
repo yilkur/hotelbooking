4c30b00e892236a7644baa9794975f62ffbae6d0
yilmazkurnazcan <yilmaz.kurnazcan@live.de>
2026-08-02 16:27:24 +0200

    add handle submit to Hotels

--- frontend/src/Hotels.jsx
+++ frontend/src/Hotels.jsx
@@ -1,26 +1,51 @@
 import {Box, Button, Grid, TextField} from "@mui/material";
 
-const handleAddHotel = () => {
+const handleSubmit = e => {
+    e.preventDefault()
+
+    const form = e.target;
+    const formData = new FormData(form);
+    const hotelname = formData.get("hotelname");
+    const ort = formData.get("ort");
+
+    alert(`hotelname: '${hotelname}'`);
+    alert(`ort: '${ort}'`);
 }
 
-const Hotels = () => (
-    <Box component="main" sx={{p: 3}}>
-        <h1>Hotels</h1>
+const Hotels = () => {
+    return (
+        <Box component="main" sx={{p: 3}}>
+            <h1>Hotels</h1>
+            <form onSubmit={handleSubmit}>
+                <Grid container spacing={2}>
 
-        <Grid container spacing={2}>
-            <Grid item xs={12} sm={6}>
-                <TextField id="name" label="Hotelname" variant="outlined" required fullWidth/>
-            </Grid>
-            <Grid item xs={12} sm={6}>
-                <TextField id="ort" label="Ort" variant="outlined" required fullWidth/>
-            </Grid>
-            <Grid item xs={12}>
-                <Button variant="contained" onClick={handleAddHotel} sx={{height: "100%"}}>
-                    Hotel hinzufügen
-                </Button>
-            </Grid>
-        </Grid>
-    </Box>
-);
+                    <Grid xs={12} sm={6}>
+                        <TextField
+                            name="hotelname"
+                            id="hotelname"
+                            label="Hotelname"
+                            variant="outlined"
+                            required
+                            fullWidth/>
+                    </Grid>
+                    <Grid xs={12} sm={6}>
+                        <TextField
+                            name="ort"
+                            id="ort"
+                            label="Ort"
+                            variant="outlined"
+                            required
+                            fullWidth/>
+                    </Grid>
+                    <Grid xs={12}>
+                        <Button variant="contained" type="submit" sx={{height: "100%"}}>
+                            Hotel hinzufügen
+                        </Button>
+                    </Grid>
+                </Grid>
+            </form>
+        </Box>
+    );
+}
 
 export default Hotels;
